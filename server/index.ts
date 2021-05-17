80a46f2d4182d03d81cd1dc7977cd93d42c13850
johnsonhuang555 <imchung0707@gmail.com>
2021-05-18 01:46:47 +0800

    add colyseus

--- server/index.ts
+++ server/index.ts
@@ -1,36 +1,22 @@
-import { createServer } from 'http';
-import { parse } from 'url';
-import next from 'next';
-import { monitor } from '@colyseus/monitor';
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
 import { Server } from 'colyseus';
+import { monitor } from '@colyseus/monitor';
+// import TicTacToe from './TicTacToe';
 
-const port = parseInt(process.env.PORT || '3000', 10);
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const port = Number(process.env.PORT || 2567);
+const app = express();
 
+app.use(cors());
+app.use(express.json());
 
+const server = http.createServer(app);
+const gameServer = new Server({ server });
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true);
-    const { pathname, query } = parsedUrl;
+// register your room handlers
+// gameServer.define('tic-tac-toe', TicTacToe);
+app.use('/colyseus', monitor());
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query);
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query);
-    } else {
-      handle(req, res, parsedUrl);
-    }
-  }).listen(port);
-
-  // tslint:disable-next-line:no-console
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  );
-});
-
-
+gameServer.listen(port);
+console.log(`Listening on ws://localhost:${port}`);
