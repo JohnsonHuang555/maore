87cb57a312586120896b6fb00077db37ea5f9d73
簡豪江 <potter80511@gmail.com>
2021-06-14 15:40:09 +0800

    改用room的重用性methods

--- server/chess/ChessGameState.ts
+++ server/chess/ChessGameState.ts
@@ -1,41 +1,67 @@
 import { Schema, ArraySchema, type } from '@colyseus/schema';
-import { PlayerState } from '../types/PlayerState';
-import { GameState } from '../../models/Room';
-import { RoomInfoState } from '../types/RoomInfoState';
 import { ChessState } from './ChessState';
+import { Side, ChessName } from '../../features/chess/model/Chess';
+import RoomState from '../room/state/RoomState';
 
 export interface ChessGame extends Schema {
-  players: ArraySchema<PlayerState>;
-  roomInfo: RoomInfoState;
   chesses: ArraySchema<ChessState>;
-  gameState: GameState;
-  activePlayer: number;
-  winningPlayer: number;
 }
 
-export default class ChessGameState extends Schema implements ChessGame {
-  @type([PlayerState])
-  players: ArraySchema<PlayerState>;
-
-  @type(RoomInfoState)
-  roomInfo: RoomInfoState;
-
-  @type('number')
-  gameState = GameState.WaitingForPlayers;
-
+export default class ChessGameState extends RoomState implements ChessGame {
   @type([ChessState])
   chesses: ArraySchema<ChessState>;
 
-  @type('number')
-  activePlayer = 0;
-
-  @type('number')
-  winningPlayer = -1;
-
   constructor() {
     super();
-    this.chesses = new ArraySchema<ChessState>();
-    this.players = new ArraySchema<PlayerState>();
-    this.roomInfo = new RoomInfoState({ roomTitle: '', maxPlayers: 0 });
+    this.chesses = this.initialChessState();
+  }
+
+  private initialChessState(): ArraySchema<ChessState> {
+    const initialArray = new Array(32);
+    const initialData: ArraySchema<ChessState> = new ArraySchema<ChessState>();
+
+    initialArray.forEach((_item, index) => {
+      const id = index + 1;
+
+      const x = (id % 8) - 1;
+      let y = 0;
+      let name = ChessName.Minion;
+
+      if (index < 8) {
+        y = 0;
+      } else if (index > 7 || index < 16) {
+        y = 1;
+      } else if (index > 15 || index < 24) {
+        y = 6;
+      } else if (index > 23 || index < 32) {
+        y = 7;
+      }
+
+      if (id === 1 || id === 8 ||  id === 25 ||  id === 32) {
+        name = ChessName.Caslte;
+      } else if (id === 2 || id === 7 ||  id === 26 ||  id === 31) {
+        name = ChessName.Kight;
+      } else if (id === 3 || id === 6 ||  id === 27 ||  id === 30) {
+        name = ChessName.Bishop;
+      } else if (id === 4 || id === 28) {
+        name = ChessName.King;
+      } else if (id === 5 || id === 29) {
+        name = ChessName.Queen;
+      } else if (id > 8 && id < 25) {
+        name = ChessName.Minion;
+      }
+
+      const side = id < 17 ? Side.Black : Side.White;
+
+      initialData.push(new ChessState({
+        id,
+        x,
+        y,
+        side,
+        name,
+      }));
+
+    });
+    return initialData;
   }
 }
