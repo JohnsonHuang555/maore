b782bd19e064f4b61092a5c86033dc1253fdb74f
Johnson Huang <imchung0707@gmail.com>
2022-04-05 21:16:58 +0800

    remove unless code

--- pages/api/game/index.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-// import { games } from '../../../server/data/Game';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(999)
-  // res.status(200).json(games);
-}
